a1849e9734e66d9149db88722d20e10b979f4372
saadzin <3613975107@qq.com>
2026-08-13 11:42:27 +0800

    ver2026-08-13

--- finance_analysis.py
+++ finance_analysis.py
@@ -12,19 +12,28 @@ print(price["Date"].is_monotonic_increasing)
 
 price["Peak"] = price["Close"].cummax()
 price["Drawdown"] = (price["Close"] / price["Peak"] - 1)
-print(price.head(10))
+price["Return"] = price["Close"].pct_change()
+price["Rolling_vol"] = price["Return"].rolling(20).std()
+price["annual_vol"] = (price["Rolling_vol"] * (252 ** 0.5))
+subprice = price.loc[price["Drawdown"] <= -0.2]
+print(subprice.head(10))
 
 max_dd = price["Drawdown"].min()
-max_dd_date = price["Drawdown"].idxmin()
+max_dd_date = price.loc[price["Drawdown"] == max_dd]
 
-print("最大回撤 %.3f" % max_dd,"最大回撤对应时间：",price.loc[max_dd_date])
+print("最大回撤 %.3f" % max_dd,"最大回撤对应时间：",max_dd_date)
 
-plt.plot(price["Date"],price["Drawdown"], label = "Drawdown")
+max_Rv_day = price["Rolling_vol"].idxmax()
+print(price.loc[max_Rv_day])
 
-plt.title("Drawdown of BYD")
+plt.plot(price["Date"],price["annual_vol"], label = "Annual_Rv")
+
+plt.title("Annual_Rv of BYD")
 plt.xlabel("Date")
-plt.ylabel("Drawdown")
+plt.ylabel("Annual_Rv")
 plt.legend()
 plt.gca().yaxis.set_major_formatter(PercentFormatter(1.0))
 
 plt.show() #test git
+
+#vol analysis test
